a8e640564918fda8a4997b6234ac7e894d04463b
Chitransh S <chitransh0210@gmail.com>
2024-04-15 04:54:32 +0530

    Alternate way of printing the 1st subsequence with sum k

---
+++ Recursion and Backtracking/4_1_0.cpp
@@ -0,0 +1,36 @@
+// Recursion on subseqences || Printing the 1st subsequences whose sum is k
+//Here the time complexity is O(n*2^n)
+// If we pass sum as a parameter we could get time complexity as O(2^n)
+#include <bits/stdc++.h>
+using namespace std;
+bool sumk(int i, vector<int> a, int arr[], int n, int k)
+{
+    if (i==n)
+    {   int sum = 0;
+        for (auto it:a) sum+=it;
+        if (sum==k)
+        {
+            for (auto v:a) cout<<v<<" ";
+            cout<<endl;
+            return true;
+        }
+        else
+        return false;
+    }
+    
+    a.push_back(arr[i]);
+    if (sumk(i + 1, a, arr, n, k)) return true;
+    a.pop_back();
+    if (sumk(i + 1, a, arr, n, k)) return true;
+    
+    return false;
+}
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 10, 11};
+    vector<int> a;
+    int k;
+    cin >> k;
+    cout<<sumk(0, a, arr, sizeof(arr) / sizeof(int), k);
+    return 0;
+}
